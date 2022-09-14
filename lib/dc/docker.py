e8b31dd86e60e878d31c8a4f46278030f0d85364
Roman Zhilkin <bins@zhilk.in>
2022-09-14 14:48:01 +0700

    1

--- lib/dc/docker.py
+++ lib/dc/docker.py
@@ -1,51 +1,71 @@
-import subprocess
 import os
+import subprocess
 import platform
 import distro
 
-def remove():
-
-    distro.distro_release_info()
-    if platform == "linux" or platform == "linux2":
-
-    elif platform == "darwin":
-    elif platform == "win32":
-
-    os.system("sudo dnf remove docker-ce docker-ce-cli containerd.io docker-compose-plugin -y")
-    os.system(
-        "sudo \
-         dnf remove \
-                  docker \
-                  docker-client \
-                  docker-client-latest \
-                  docker-common \
-                  docker-latest \
-                  docker-latest-logrotate \
-                  docker-logrotate \
-                  docker-selinux \
-                  docker-engine-selinux \
-                  docker-engine \
-                  -y"
-    )
+operation_system = platform.system()
+operation_system_release = platform.release()
+operation_system_name = distro.id()
 
 
 def install():
-    os.system("sudo sh "+os.path.join("lib", "dc", "get_docker.sh"))
+    if platform.system() == "win32":
+        pass
+    elif platform.system() == "Linux":
+        pass
+    elif platform.system() == "darwin":
+        pass
+    else:
+        print("UNKNOWN OPERATION SYSTEM"+platform.system())
 
-
-def current_version():
-    output = None
-    try:
-        output = subprocess.check_output(['docker -v'], shell=True, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as Error:
-        if Error.args[0] == 127:
-            output = None
-        elif Error.args[0] == 1:
-            output = None
-        else:
-            print(Error)
-    finally:
-        if output is None:
-            return output
-        else:
-            return str(output).rstrip().replace("b'", "").replace(",", "").split(" ")[2]
+# import subprocess
+# import os
+# import platform
+# import distro
+#
+# def remove():
+#
+#     distro.distro_release_info()
+#     if platform == "linux" or platform == "linux2":
+#
+#     elif platform == "darwin":
+#     elif platform == "win32":
+#
+#     os.system("sudo dnf remove docker-ce docker-ce-cli containerd.io docker-compose-plugin -y")
+#     os.system(
+#         "sudo \
+#          dnf remove \
+#                   docker \
+#                   docker-client \
+#                   docker-client-latest \
+#                   docker-common \
+#                   docker-latest \
+#                   docker-latest-logrotate \
+#                   docker-logrotate \
+#                   docker-selinux \
+#                   docker-engine-selinux \
+#                   docker-engine \
+#                   -y"
+#     )
+#
+#
+# def install():
+#     os.system("sudo sh "+os.path.join("lib", "dc", "get_docker.sh"))
+#
+#
+# def current_version():
+#     output = None
+#     try:
+#         output = subprocess.check_output(['docker -v'], shell=True, stderr=subprocess.STDOUT)
+#     except subprocess.CalledProcessError as Error:
+#         if Error.args[0] == 127:
+#             output = None
+#         elif Error.args[0] == 1:
+#             output = None
+#         else:
+#             print(Error)
+#     finally:
+#         if output is None:
+#             return output
+#         else:
+#             return str(output).rstrip().replace("b'", "").replace(",", "").split(" ")[2]
--- lib/server.py
+++ lib/server.py
@@ -1,22 +1,24 @@
-# from dc import docker, dockerCompose
-#
+from dc import docker, dockerCompose
+
+docker.install()
+
 # if docker.current_version() is None:
 #     docker.install()
 # if dockerCompose.current_version() is None:
 #     dockerCompose.install()
-#
-# docker.remove()
-import os
-import platform
-import sys
 
-import distro
-print(os.name)
-print(sys.platform)
-print(platform.system())
-print(platform.release())
-print(distro.id())
-print(distro.os_release_info())
-print(distro.info())
-print(distro.main())
-print(distro.distro_release_info())
+# docker.remove()
+# import os
+# import platform
+# import sys
+#
+# import distro
+# print(os.name)
+# print(sys.platform)
+# print(platform.system())
+# print(platform.release())
+# print(distro.id())
+# print(distro.os_release_info())
+# print(distro.info())
+# print(distro.main())
+# print(distro.distro_release_info())

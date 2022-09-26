85c1aac74a1d81e1b279e5a55dea8e7babd04615
Roman Zhilkin <bins@zhilk.in>
2022-09-26 10:09:36 +0700

    q

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 # shellcheck disable=SC2068
 if [ -z "$PYTHONPATH" ]; then
         export PYTHONPATH="src:$PYTHONPATH"
+        export PYTHONPATH="src/libs:$PYTHONPATH"
         echo "SET PYTHONPATH LIB - OK"
 fi
 # echo "$PYTHONPATH"
--- src/server.py
+++ src/server.py
@@ -1,8 +1,7 @@
 import sys
 from src.libs import container
+
 print(sys.argv)
-
-
 
 container.docker.print_test()
 container.docker_compose.print_test()

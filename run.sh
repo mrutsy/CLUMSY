0f341c85ae80cdf8072cfd20ba011a514f5cd170
Roman Zhilkin <bins@zhilk.in>
2022-09-26 10:12:42 +0700

    q

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 # shellcheck disable=SC2068
 if [ -z "$PYTHONPATH" ]; then
         export PYTHONPATH="src:$PYTHONPATH"
-        export PYTHONPATH="src/libs:$PYTHONPATH"
         echo "SET PYTHONPATH LIB - OK"
 fi
 # echo "$PYTHONPATH"

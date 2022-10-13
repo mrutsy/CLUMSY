99fe924653bbf6bcd02b3c9b7510556e13998781
Roman Zhilkin <bins@zhilk.in>
2022-10-13 15:42:49 +0700

    Add Docker libs

--- src/libs/container/docker.py
+++ src/libs/container/docker.py
@@ -1,6 +1,71 @@
-class Docker (object):
-    pass
+class Docker(object):
+
+    def docker_install(self):
+        pass
+
+    def docker_remove(self):
+        pass
+
+    def docker_version(self):
+        pass
+
+    def compose_install(self):
+        pass
+
+    def compose_remove(self):
+        pass
+
+    def compose_version(self):
+        pass
+
+    def compose_up(self):
+        pass
+
+    def compose_down(self):
+        pass
+
+    def image_search(self):
+        pass
+
+    def image_pull(self):
+        pass
+
+    def image_import(self):
+        pass
+
+    def image_list(self):
+        pass
+
+    def container_create(self):
+        pass
+
+    def container_remove(self):
+        pass
+
+    def container_rename(self):
+        pass
+
+    def container_list(self):
+        pass
+
+    def container_list_all(self):
+        pass
+
+    def container_start(self):
+        pass
+
+    def container_stop(self):
+        pass
+
+    def container_restart(self):
+        pass
+
+    def container_pause(self):
+        pass
+
+    def container_unpause(self):
+        pass
 
 
-class DockerCompose (object):
-    pass
+if __name__ == "__main__":
+    Docker().image_import()
